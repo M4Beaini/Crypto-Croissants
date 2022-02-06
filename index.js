bdb68dfb549923b2a1ecc2c0cdaefc572d564043
Maher Beaini <99055435+M4Beaini@users.noreply.github.com>
2022-02-06 05:23:55 +0000

    1) Removed old code 2) Updated port. From 3000 to 80

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 
 const express = require('express');//Set up the express module
 const app = express();
-const port = 3000;
-// const router = express.Router();
-//const path = require('path')//Include the Path module
+const port = 80;
+
 
 app.use (express.json());
 app.listen (port,() => {
@@ -12,32 +11,6 @@ app.listen (port,() => {
 });
 
 
-/*
-//Set up the Express router
-router.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '/index.html/index.html'));
-});
-app.use('/', router);
-
-//Navigate your website
-//if they go to '/lol'
-router.get('/lol', function(req, res){
-  res.sendFile(path.join(__dirname, '/lol.html'));
-});
-app.use('/lol', router);
-//404 Error
-app.use(function(req, res, next) {
-    res.status(404);
-    res.sendFile(__dirname + '/404.html');
-});
-
-
-//set up the Express server to listen on port 3000 and logs some messages when the server is ready
-let server = app.listen(3000, function(){
-  console.log("App server is running on port 3000");
-});
-
-*/
 
 // HELLO WORLD CODE
 
